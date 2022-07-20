88605d9dd1f16106df8a9dcb0eb712baf4f333ca
mauserne <52377363+mauserne@users.noreply.github.com>
2022-07-21 06:25:38 +0900

    test
    테스트

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitch-chat-rapid-copier
+ 원클릭 트위치 채팅 복사
---
+++ background.js
@@ -0,0 +1,25 @@
+let color = '#aaaaaa';
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url){
+      console.log(tab)
+      chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      url: tab.url,
+    }, function(response){
+      console.log(response)
+    });
+  }
+});
+
+/*
+if (activeTab.url.includes("twitch.com/") && currentVideo) {
+    chrome.runtime.onInstalled.addListener(() => {
+      chrome.storage.sync.set({ color });
+      console.log('Default background color set to %cgreen', `color: ${color}`);
+  });
+} else {
+  const container = document.getElementsByClassName("container")[0];
+
+  container.innerHTML = '<div class="title">This is not a youtube video page.</div>';
+}
+*/
