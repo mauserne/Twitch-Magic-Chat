6cc33ac9e20d6bced9686e46ef95eae1e72255a2
mauserne <tlrhf2@gmail.com>
2022-08-03 09:29:25 +0900

    최초실행시 초기값 설정 (on/off)

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
-let color = "#aaaaaa";
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    console.log(
+      "Thank you for download Twitch Magic Chat! default value is 'active' "
+    );
+    chrome.storage.local.set({ switch: true }, () =>
+      console.log("default is true")
+    );
+  }
+});
+
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   console.log(tab);
   if (tab.url) {
@@ -15,48 +25,3 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     );
   }
 });
-
-/*
-let previousUrl = "";
-const urlObserver = new MutationObserver(function (mutations) {
-  if (location.href !== previousUrl) {
-    previousUrl = location.href;
-    console.log(`URL changed to ${location.href}`);
-
-    observer.disconnect();
-    interval();
-  }
-});
-urlObserver.observe(document, { subtree: true, childList: true });
-*/
-
-/*
-// function that injects code to a specific tab
-function injectScript(tabId) {
-  console.log("inject");
-  chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    files: ["content-script.js"],
-  });
-}
-
-// adds a listener to tab change
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // check for a URL in the changeInfo parameter (url is only added when it is changed)
-  if (changeInfo.url) {
-    // calls the inject function
-    injectScript(tabId);
-  }
-});
-
-if (activeTab.url.includes("twitch.com/") && currentVideo) {
-    chrome.runtime.onInstalled.addListener(() => {
-      chrome.storage.sync.set({ color });
-      console.log('Default background color set to %cgreen', `color: ${color}`);
-  });
-} else {
-  const container = document.getElementsByClassName("container")[0];
-
-  container.innerHTML = '<div class="title">This is not a youtube video page.</div>';
-}
-*/
